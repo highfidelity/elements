01b90fbe18f76137bc694bcd1f84093e9f284977
Kerry Ivan Kurian <kkurian@gmail.com>
2018-02-19 17:47:35 -0800

    Parametrize blockchain type

--- contrib/highfidelity/alice_and_bob.py
+++ contrib/highfidelity/alice_and_bob.py
@@ -5,13 +5,13 @@ from .wallet import Wallet, SEED_AMOUNT
 
 
 @contextlib.contextmanager
-def alice_and_bob():
-    with Elements.node('master') as master_node:
+def alice_and_bob(blockchain=Elements):
+    with blockchain.node('master') as master_node:
         Wallet.master_node = master_node
-        with Elements.node('alice') as node_alice:
+        with blockchain.node('alice') as node_alice:
             alice = Wallet(node_alice)
             alice.seed(SEED_AMOUNT)
-            with Elements.node('bob') as node_bob:
+            with blockchain.node('bob') as node_bob:
                 bob = Wallet(node_bob)
                 bob.seed(SEED_AMOUNT)
                 yield (alice, bob)
