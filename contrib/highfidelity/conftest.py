a169bd1647b5afeec8569bc69a35645eb739ed51
Kerry Ivan Kurian <kkurian@gmail.com>
2018-02-20 22:13:06 -0800

    Disable eos option for now

--- contrib/highfidelity/conftest.py
+++ contrib/highfidelity/conftest.py
@@ -4,12 +4,13 @@
 from .test_framework.blockchain import Elements, EOS
 
 
-ALL_BLOCKCHAINS = [Elements, EOS]
+# ALL_BLOCKCHAINS = [Elements, EOS]
+ALL_BLOCKCHAINS = [Elements]
 
 
 def pytest_addoption(parser):
     parser.addoption('--elements', action='store_true', help='limit tests to Elements')  # noqa: E501
-    parser.addoption('--eos', action='store_true', help='limit tests to EOS')
+    # parser.addoption('--eos', action='store_true', help='limit tests to EOS')
 
 
 def pytest_generate_tests(metafunc):
@@ -18,8 +19,8 @@ def pytest_generate_tests(metafunc):
         blockchains = list()
         if config.getoption('elements'):
             blockchains.append(Elements())
-        if config.getoption('eos'):
-            blockchains.append(EOS())
+        # if config.getoption('eos'):
+        #     blockchains.append(EOS())
         if not blockchains:
             blockchains = [blockchain() for blockchain in ALL_BLOCKCHAINS]
         metafunc.parametrize('blockchain', blockchains)
