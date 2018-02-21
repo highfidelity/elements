846683d3dce4080f74d03125c2afcb0791e25832
Kerry Ivan Kurian <kkurian@gmail.com>
2018-02-21 15:05:26 -0800

    Remove cruft

--- contrib/highfidelity/test_framework/blockchain/blockchain.py
+++ contrib/highfidelity/test_framework/blockchain/blockchain.py
@@ -1,19 +1,7 @@
 import contextlib
 
-from ..error import Error
-
 
 class Blockchain:
-    class CannotAddTransactionError(Error):
-        pass
-
-    @classmethod
-    @contextlib.contextmanager
-    def node_pair(cls):
-        with cls.node('master') as master:
-            with cls.node('slave') as slave:
-                yield master, slave
-
     @classmethod
     @contextlib.contextmanager
     def node(cls, daemon_name):
