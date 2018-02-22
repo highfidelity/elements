45b0ed034c1d162a4e17e3de0a9ab78bc9d8e481
Kerry Ivan Kurian <kkurian@gmail.com>
2018-02-22 14:58:48 -0800

    Update interface specification

--- contrib/highfidelity/test_framework/blockchain/blockchain.py
+++ contrib/highfidelity/test_framework/blockchain/blockchain.py
@@ -4,5 +4,12 @@ import contextlib
 class Blockchain:
     @classmethod
     @contextlib.contextmanager
-    def node(cls, daemon_name):
+    def node(
+        cls,
+        name,
+        _warm_up_master=False,
+        _ensure_signing_key=True,
+        _pdb_on_exception=True,
+        _debug=True
+    ):
         raise NotImplementedError
